2f2ae39cdfad6161e949dac5a8ad4a54ee82cbeb
Ashik Meeran Mohideen S <ashikmeeranmohideen@gmail.com>
2020-12-03 13:25:23 +0530

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Flask-Tutorials
+## Overview:
+  * This tutorial is about implementing OAuth login (Google, Github) in your Flask app using Authlib.
+  * To view the Youtube demonstration of this tutorial [click here]()
+
+## Prerequisites:
+  * Python version 3.8
+
+## Getting started:
+  1. Install pipenv by running:
+  ```bash
+    $> pip install pipenv
+  ```
+  2. Installing dependencies:
+  ```bash
+    $> pipenv install -r Pipfile.lock
+  ``` 
+  ## Note:
+  * > Pipfile varies with respect to the operating system.
+
+  * > If you use other operating system than windows, it is recommended to install the dependencies manually one by one.
+
+  ## Useful Links:
+  * [Youtube Demonstration]()
+  * [Authlib documentation](https://docs.authlib.org/en/latest/)
+  * [Registering your OAuth app with Google](https://console.developers.google.com/apis/dashboard)
+  * [Registering your OAuth app with Github](https://github.com/settings/developers)
+
+
+  
---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, url_for, redirect
+from authlib.integrations.flask_client import OAuth
+
+app = Flask(__name__)
+
+oauth = OAuth(app)
+
+app.config['SECRET_KEY'] = "THIS SHOULD BE SECRET"
+app.config['GOOGLE_CLIENT_ID'] = "<your google client id>"
+app.config['GOOGLE_CLIENT_SECRET'] = "<your google client secret>"
+app.config['GITHUB_CLIENT_ID'] = "<your github client id>"
+app.config['GITHUB_CLIENT_SECRET'] = "<your github client secret>"
+
+google = oauth.register(
+    name = 'google',
+    client_id = app.config["GOOGLE_CLIENT_ID"],
+    client_secret = app.config["GOOGLE_CLIENT_SECRET"],
+    access_token_url = 'https://accounts.google.com/o/oauth2/token',
+    access_token_params = None,
+    authorize_url = 'https://accounts.google.com/o/oauth2/auth',
+    authorize_params = None,
+    api_base_url = 'https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs = {'scope': 'openid email profile'},
+)
+
+
+github = oauth.register (
+  name = 'github',
+    client_id = app.config["GITHUB_CLIENT_ID"],
+    client_secret = app.config["GITHUB_CLIENT_SECRET"],
+    access_token_url = 'https://github.com/login/oauth/access_token',
+    access_token_params = None,
+    authorize_url = 'https://github.com/login/oauth/authorize',
+    authorize_params = None,
+    api_base_url = 'https://api.github.com/',
+    client_kwargs = {'scope': 'user:email'},
+)
+
+
+# Default route
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+# Google login route
+@app.route('/login/google')
+def google_login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('google_authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+# Google authorize route
+@app.route('/login/google/authorize')
+def google_authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo').json()
+    print(f"\n{resp}\n")
+    return "You are successfully signed in using google"
+
+
+# Github login route
+@app.route('/login/github')
+def github_login():
+    github = oauth.create_client('github')
+    redirect_uri = url_for('github_authorize', _external=True)
+    return github.authorize_redirect(redirect_uri)
+
+
+# Github authorize route
+@app.route('/login/github/authorize')
+def github_authorize():
+    github = oauth.create_client('github')
+    token = github.authorize_access_token()
+    resp = github.get('user').json()
+    print(f"\n{resp}\n")
+    return "You are successfully signed in using github"
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
